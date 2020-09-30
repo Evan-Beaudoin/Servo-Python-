d64552a05fc4b4bff6852e1331c9550e7374f4a4
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-09-30 17:49:21 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from microbit import *
+
+#Created By: your name
+#Created on: 
+
+#This program: Uses Servo
+
+pin1.set_analog_period(10)
+
+while True:
+ 
+ 
+ pin1.write_analog(60)
+ sleep(1000)
+
+ pin1.write_analog(220) 
+ sleep(1000)
+ 
